f36eac2385c3c626ae70b1ccf64ba2cede9da6a3
ExplodingONC <zhangxinyue9825@hotmail.com>
2022-10-14 12:08:38 +0800

    add SPI

--- LiDAR_test.py
+++ LiDAR_test.py
@@ -11,9 +11,9 @@ import scipy.constants
 # import I/O modules
 import RPi.GPIO as GPIO
 import smbus2
+import spidev
 
 # parameters
-extCDLL = ctypes.CDLL("/home/ExplodingONC/Projects_Python/direct_GPIO.so")
 width = int(96)  # not include header pixel
 height = int(72)
 pixel_count = (width + 1) * height
@@ -21,13 +21,6 @@ pixel_count = (width + 1) * height
 # timestamp
 date = datetime.datetime.now().astimezone()
 print(date.strftime("%Y-%m-%d %H:%M:%S.%f %Z %z"))
-
-# GPIO register hook
-if extCDLL.GPIO_reg_setup():
-    print("GPIO hook failed!")
-    sys.exit()
-else:
-    print("GPIO register hooked.")
 
 # setup IIC bus
 try:
@@ -46,13 +39,29 @@ except Exception as err:
 else:
     print("I2C initialized.")
 
+# setup SPI bus
+try:
+    spi_channel = 0
+    spi_device_MCU = 1
+    spi = spidev.SpiDev()
+    spi.open(spi_channel, spi_device_MCU)
+except Exception as err:
+    print("Error:", err)
+    print("SPI initialization failed!")
+    sys.exit()
+else:
+    spi.max_speed_hz = 10000000
+    spi.mode = 0
+    print("SPI initialized.")
+
 # setup GPIOs
 try:
     # GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.OUT)  # reset lock (N)
-    GPIO.setup(27, GPIO.OUT)  # partial reset (N)
-    GPIO.setup(22, GPIO.OUT)  # master reset (N)
+    pin_sensor_rst = 4
+    pin_mcu_rst = 23
+    GPIO.setup(pin_sensor_rst, GPIO.OUT)  # sensor reset (P)
+    GPIO.setup(pin_mcu_rst, GPIO.OUT)  # MCU reset (N)
 except Exception as err:
     print("Error:", err)
     print("GPIO initialization failed!")
@@ -60,17 +69,23 @@ except Exception as err:
 else:
     print("GPIO initialized.")
 
-try:
+# reset devices
+GPIO.output(pin_sensor_rst, 1)
+GPIO.output(pin_mcu_rst, 0)
+time.sleep(0.01)
+GPIO.output(pin_sensor_rst, 0)
+GPIO.output(pin_mcu_rst, 1)
+time.sleep(0.1)
 
-    # reset FIFO
-    GPIO.output(22, 0)
-    time.sleep(0.01)
-    GPIO.output(22, 1)
-    time.sleep(0.01)
+
+# main program
+print()
+try:
 
     # LiDAR setup through IIC
     try:
         i2c1.write_byte_data(i2c_address_lidar, 0x00, 0b11100011)  # stop operation
+        i2c1.write_byte_data(i2c_address_lidar, 0x07, 0b11000000)  # unknown?
         i2c1.write_byte_data(i2c_address_lidar, 0x08, 0x00)  # ext_reset
         i2c1.write_byte_data(i2c_address_lidar, 0x09, 0x40)
         i2c1.write_byte_data(i2c_address_lidar, 0x0A, 0x00)  # H_pixel_num
@@ -78,7 +93,7 @@ try:
         i2c1.write_byte_data(i2c_address_lidar, 0x0C, 0x00)  # V_pixel_num
         i2c1.write_byte_data(i2c_address_lidar, 0x0D, 0x48)
         i2c1.write_byte_data(i2c_address_lidar, 0x0E, 0x25)  # HST_offset
-        i2c1.write_byte_data(i2c_address_lidar, 0x0F, 0b00110000)  # light_pattern
+        i2c1.write_byte_data(i2c_address_lidar, 0x0F, 0b10110111)  # light_pattern
         i2c1.write_byte_data(i2c_address_lidar, 0x10, 0xFF)  # frame blanking
         i2c1.write_byte_data(i2c_address_lidar, 0x11, 0x00)
         i2c1.write_byte_data(i2c_address_lidar, 0x12, 0x08)  # ADC_delay_cfg
@@ -86,13 +101,13 @@ try:
         i2c1.write_byte_data(i2c_address_lidar, 0x14, 0x80)
         i2c1.write_byte_data(i2c_address_lidar, 0x15, 0x00)
         i2c1.write_byte_data(i2c_address_lidar, 0x16, 0x01)  # Ndata
-        i2c1.write_byte_data(i2c_address_lidar, 0x17, 0xFF)  # VTX1
-        i2c1.write_byte_data(i2c_address_lidar, 0x18, 0xFF)  # VTX2
+        i2c1.write_byte_data(i2c_address_lidar, 0x17, 0x7F)  # VTX1
+        i2c1.write_byte_data(i2c_address_lidar, 0x18, 0x7F)  # VTX2
         i2c1.write_byte_data(i2c_address_lidar, 0x19, 0x00)  # VTX3
         i2c1.write_byte_data(i2c_address_lidar, 0x1A, 0x04)
-        i2c1.write_byte_data(i2c_address_lidar, 0x1B, 0xFF)  # light_pulse_width
+        i2c1.write_byte_data(i2c_address_lidar, 0x1B, 0x7F)  # light_pulse_width
         i2c1.write_byte_data(i2c_address_lidar, 0x1D, 0x01)  # light_pulse_offset
-        i2c1.write_byte_data(i2c_address_lidar, 0x1F, 0x7F)  # P4_delay
+        i2c1.write_byte_data(i2c_address_lidar, 0x1F, 0x3F)  # P4_delay
         i2c1.write_byte_data(i2c_address_lidar, 0x20, 0b00001001)  # L/A, Light_pulse_half_delay, H_pixel_blanking
         i2c1.write_byte_data(i2c_address_lidar, 0x21, 0x00)  # T1 (linear only)
         i2c1.write_byte_data(i2c_address_lidar, 0x22, 0x00)  # PHIS (linear only)
@@ -115,14 +130,12 @@ try:
         print(" - I2C data sent.")
     time.sleep(0.25)
 
+    while 1:
+        pass
+
+    """
     # GPIO frame read (through FIFO)
-    GPIO.output(17, 0)  # disable FIFO reset signal
-    time_start = time.perf_counter_ns()
-    extCDLL.read_area_frame_fifo.restype = numpy.ctypeslib.ndpointer(dtype=ctypes.c_uint32, shape=(4, height, 2 * (width + 1)))
-    data_stream = extCDLL.read_area_frame_fifo(ctypes.c_int(width + 1), ctypes.c_int(height))
-    time_end = time.perf_counter_ns()
-    print("Total time of %dx%d pixels: %fs." % (width + 1, height, 1e-9 * (time_end - time_start)))
-    GPIO.output(17, 1)  # enable FIFO reset
+    data_stream = 0
 
     # LiDAR data processing
     depth_map = numpy.zeros((height, width), dtype=int, order='C')
@@ -159,9 +172,11 @@ try:
             # WIP, lacks calibration process now
     print(depth_map)
     print(intensity_map)
+    """
 
 finally:
-
     # GC
     GPIO.cleanup()
+    spi.close()
+    print("\n - GPIO clean up.")
     sys.exit(0)
